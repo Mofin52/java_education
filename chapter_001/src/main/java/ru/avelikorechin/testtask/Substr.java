86bda37e145422386965aba624fbadbb3309cd92
Mofin52 <a.velikorechin@gmail.com>
2017-01-24 14:34:46 +0300

    Remade and simplified contains method

--- chapter_001/src/main/java/ru/avelikorechin/testtask/Substr.java
+++ chapter_001/src/main/java/ru/avelikorechin/testtask/Substr.java
@@ -1,5 +1,4 @@
 package ru.avelikorechin.testtask;
-import java.util.Arrays;
 /**
 	* Class to work with substrings.
 	* @author Alexander Velikorechin
@@ -13,19 +12,21 @@ public class Substr {
 		* @return true — sub is substring of origin, false — sub is not in origin
 	*/
 	public boolean contains(String origin, String sub) {
-		char[] subArr = new char[sub.length()];
-		for (int i = 0; i < sub.length(); i++) {
-			subArr[i] = sub.charAt(i);
-		}
-		char[][] originArr = new char[origin.length() - sub.length() + 1][sub.length()];
-		for (int i = 0; i < (origin.length() - sub.length() + 1); i++) {
-			for (int j = 0; j < sub.length(); j++) {
-				originArr[i][j] = origin.charAt(i + j);
-			}
-		}
-		for (int i = 0; i < originArr.length; i++) {
-			if (Arrays.equals(originArr[i], subArr)) {
-				return true;
+		char[] subArr = sub.toCharArray();
+		char[] originArr = origin.toCharArray();
+		int count = 0;
+		if (originArr.length >= subArr.length) {
+			for (int i = 0; i < (originArr.length - subArr.length + 1); i++) {
+				for (int j = 0; j < subArr.length; j++) {
+					if (originArr[i + j] == subArr[j]) {
+						count++;
+						if (count == subArr.length) {
+							return true;
+						}
+					} else {
+						count = 0;
+					}
+				}
 			}
 		}
 	return false;
