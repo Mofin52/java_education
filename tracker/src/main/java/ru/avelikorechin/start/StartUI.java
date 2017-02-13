f456a497dafaa0e2112a87966c4826560c5fd23b
Mofin52 <a.velikorechin@gmail.com>
2017-02-13 21:58:29 +0300

    Tuned pom.xml to make tracker.jar executable

---
+++ tracker/src/main/java/ru/avelikorechin/start/StartUI.java
@@ -0,0 +1,16 @@
+package ru.avelikorechin.start;
+
+/**
+ * Tracker start class.
+ * @author Alexander Velikorechin
+ * @since 13.02.2017
+ */
+public class StartUI {
+    /**
+     * Entering point.
+     * @param args of main
+     */
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+    }
+}
